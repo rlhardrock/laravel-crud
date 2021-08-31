e7f49134db8808f88bbd4014636c795e1294c4be
Louis Rey <rlhardrock@gmail.com>
2021-08-31 01:35:57 -0500

    install datatable plug

--- resources/views/animal/index.blade.php
+++ resources/views/animal/index.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.tempbase')
 
-@section('crud-content')
-<a href="animal/create" class="btn btn-primary">OTHER ANIMAL</a>
+@section('css')
+<link hfer="https://cdn.datatables.net/1.11.0/css/dataTables.bootstrap5.min.css" rel="stylesheet"/>
+@endsection
 
-<table class="table table-dark table-striped mt-4">
+@section('crud-content')
+
+<table id="animals" class="table table-borderer table-dark table-striped" style="width:100%">
     <thead>
         <tr>
             <th scope="col">ID</th>
@@ -24,7 +27,7 @@
             <td>{{$animal->country}}</td>
             <td>
                 <form action="{{route('animal.destroy',$animal->id)}}" method="POST">
-                    
+
                     <a href="/animal/{{$animal->id}}/edit" class="btn btn-success">EDIT</a>
                     @csrf
                     @method('DELETE')
@@ -37,4 +40,23 @@
     </tbody>
 </table>
 
+<a href="animal/create" class="btn btn-primary mb-3">ANIMAL</a>
+
+@section('js')
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.11.0/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.0/js/dataTables.bootstrap5.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#animals').DataTable({
+                "lengthMenu": [[5,10,50,-1],[5,10,50,"All"]]
+            });
+        });
+    </script>
 @endsection
+
+@endsection
+
+
+
+
