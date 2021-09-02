429abaf4c3cc2ab16d25b90ab42926bf75b2621f
Louis Rey <rlhardrock@gmail.com>
2021-09-02 11:43:16 -0500

    refact dashboard in white

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,12 +7,12 @@
 @stop
 
 @section('content')
-<x-app-layout>
-    {{-- <x-slot name="header">
+{{-- <x-app-layout>
+    <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-    </x-slot> --}}
+    </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -21,11 +21,11 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-app-layout> --}}
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
 @stop
 
 @section('js')
